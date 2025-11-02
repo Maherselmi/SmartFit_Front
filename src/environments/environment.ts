71de397d9e10b0069263aa01e17348ce3f67d28e
azizowski10 <mohamedaziz.grissa@esprit.tn>
2025-11-02 16:05:21 +0200

    added groq api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  groqApiKey: '',
+  groqApiKey: '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',
   groqApiUrl: 'https://api.groq.com/openai/v1/chat/completions'
 };
 
